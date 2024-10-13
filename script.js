3cb23e43ac99eaf70f57448a070480f3431dbd6f
etai89 <senioretai@gmail.com>
2024-10-13 18:18:09 +0300

    test

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("leadForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+  
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const webHook = 'bywgxf1hhq4j76jeg7lejny4ct4gi7cn@hook.eu2.make.com'
+    // Replace with your Make.com webhook URL
+    // const webhookURL = `https://hook.us1.make.com/${webHook}`;
+    const webhookURL = webHook
+
+  
+    fetch(webhookURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, email })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Lead submitted successfully:", data);
+    })
+    .catch(error => {
+      console.error("Error submitting lead:", error);
+    });
+  });
+  
