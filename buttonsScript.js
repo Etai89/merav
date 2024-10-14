158102d668e3ea15738d85093e57548e44af5420
etai89 <senioretai@gmail.com>
2024-10-14 16:28:25 +0300

    links

--- buttonsScript.js
+++ buttonsScript.js
@@ -8,14 +8,14 @@ $(document).ready(() => {
     const buttons = [whatsapp, callMe, facebook, instagram, gmail]
 
 
-    for (let btn = 0; btn<buttons.length; btn++) {
-        $(buttons[btn]).on('mouseover', ()=>{
+    for (let btn = 0; btn < buttons.length; btn++) {
+        $(buttons[btn]).on('mouseover', () => {
             $(buttons[btn]).css('width', '60px')
             $(buttons[btn]).css('height', '60px')
         })
     }
-    for (let btn = 0; btn<buttons.length; btn++) {
-        $(buttons[btn]).on('mouseout', ()=>{
+    for (let btn = 0; btn < buttons.length; btn++) {
+        $(buttons[btn]).on('mouseout', () => {
             $(buttons[btn]).css('width', '50px')
             $(buttons[btn]).css('height', '50px')
         })
@@ -25,6 +25,40 @@ $(document).ready(() => {
 
     })
 
+    // triggering buttons:
+
+    // links:
+    const faceB = 'https://www.facebook.com/meravsixso'
+    const insta = 'https://www.instagram.com/meravsixou'
+    const mail = 'meravsixou22@gmail.com'
+
+    function call() {
+        // Replace with your phone number
+        window.location.href = "tel:+972548188376";
+    }
+
+    callMe.on('click', () => {
+        call()
+    })
+    // Function to open WhatsApp chat
+    function whatsAppMe() {
+        // Replace with your phone number in the WhatsApp link
+        const phoneNumber = "972548188376";
+        const message = encodeURIComponent("היי אני מעוניינת לשמוע פרטים,");
+        window.location.href = `https://wa.me/${phoneNumber}?text=${message}`;
+    }
 
 
+    whatsapp.on('click', () => {
+        whatsAppMe()
+    })
+
+    facebook.on('click', () => {
+        window.open(faceB, '_blank')
+    })
+
+    instagram.on('click', () => {
+        window.open(insta, '_blank')
+    })
+    
 })
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+
+
     $("#leadForm").on("submit", function(event) {
       event.preventDefault()
   
       const name = $("#name").val();
       const email = $("#email").val();
+      const tel = $('#tel').val()
       
       const webhookURL = "https://hook.eu2.make.com/f76rfii7hrg74r9iovbmoeus4nu2jqmd";
       
@@ -13,7 +16,8 @@ $(document).ready(function() {
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify({
              name: name,
-             email: email 
+             email: email,
+             tel: tel
             }),
         success: function(data) {
           console.log("Lead submitted successfully:", data)
